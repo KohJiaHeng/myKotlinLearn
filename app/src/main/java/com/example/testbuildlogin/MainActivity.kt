41bcb1509e601aefda2185f56d03ea0d8c477887
KohJiaHeng <kohjiaheng23@gmail.com>
2026-06-29 23:14:08 +0800

    Done

--- app/src/main/java/com/example/testbuildlogin/MainActivity.kt
+++ app/src/main/java/com/example/testbuildlogin/MainActivity.kt
@@ -8,35 +8,34 @@ import androidx.activity.viewModels
 import com.example.testbuildlogin.model.UserCredential
 import com.example.testbuildlogin.viewmodel.LoginViewModel
 import android.content.Intent
+import com.example.testbuildlogin.databinding.MyloginpageBinding
 
 
 class MainActivity : ComponentActivity() {
     private val loginViewModel: LoginViewModel by viewModels()
+    private lateinit var binding: MyloginpageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = MyloginpageBinding.inflate(layoutInflater)
         setContentView(R.layout.myloginpage)
 
-        val usernameEdit = findViewById<EditText>(R.id.usernameInput)
-        val passwordEdit = findViewById<EditText>(R.id.passwordInput)
-        val loginBtn = findViewById<Button>(R.id.loginButton)
+        binding.loginButton.setOnClickListener {
+                val username = binding.usernameInput.text.toString()
+                val password = binding.passwordInput.text.toString()
+                val userCredential = UserCredential(username = username, password = password)
+                val canLogIn = loginViewModel.isLoginValid(userCredential)
 
-        loginBtn.setOnClickListener {
-            val username = usernameEdit.text.toString()
-            val password = passwordEdit.text.toString()
-            val userCredential = UserCredential(username = username, password = password)
-            val canLogIn = loginViewModel.isLoginValid(userCredential)
-
-            if (canLogIn) {
-                goToUserPage(username = userCredential.username)
-            } else {
-                Toast.makeText(
-                    this,
-                    "Different Username ${userCredential.username}",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-
+                if (canLogIn) {
+                    goToUserPage(username = userCredential.username)
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Different Username ${userCredential.username}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
         }
+
 
     }
     private fun goToUserPage(username: String) {
